40366e19000955ffd3acc2c2c6cef1c7e5c700a1
Dan Mønster <dan@m0nster.dk>
2017-07-06 17:03:54 +0200

    Skeleton function for computing transition matrix.

---
+++ R/transition_matrix.R
@@ -0,0 +1,12 @@
+#' Compute the transition matrix.
+#'
+#' The transition matrix is computed from the aggression matrix. The element T[i,j] of
+#' the transition matrix is the probability that individual i attacks individual j.
+#'
+#' @param A aggression matrix
+#' @param eps the epsilon parameter
+#' @return The transition matrix
+
+ComputeTransitionMatrix <- function(A, eps = 1e-6) {
+
+}
