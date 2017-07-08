4279c16f086de6b8585a96c91644818d1d48d240
Dan Mønster <dan@m0nster.dk>
2017-07-08 22:49:39 +0200

    Added example to function documentation.

--- R/containment.R
+++ R/containment.R
@@ -9,7 +9,11 @@
 #' @export
 #'
 #' @examples
+#' trans.mat <- array(c(0, 0.5, 0.2, 1, 0, 0.8, 0, 0.5, 0), dim = c(3, 3))
+#' containment(trans.mat)
+#'
 containment <- function(trans.mat) {
   eig.vals <- eigen(trans.mat, only.values = TRUE)
   unique(abs(eig.vals$values))[2]
+  #TODO(danm0nster): check that there are at least two unique eigenvalues
 }
