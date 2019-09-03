6b1ea5d70ddd7c7951be615721343277490f8147
Dan Mønster <dan@m0nster.dk>
2019-09-03 14:02:54 +0200

    Added lintr to check code

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,12 @@
 library(testthat)
 library(domstruc)
+library(lintr)
 
 test_check("domstruc")
+
+if (requireNamespace("lintr", quietly = TRUE)) {
+  context("lints")
+  test_that("Package Style", {
+    lintr::expect_lint_free()
+  })
+}
