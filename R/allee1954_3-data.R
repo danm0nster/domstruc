2a449a27992db2daee3524f71520e00122364987
Dan Mønster <dan@m0nster.dk>
2019-09-06 16:13:26 +0200

    Added sample data: allee1954_3

---
+++ R/allee1954_3-data.R
@@ -0,0 +1,25 @@
+#' Allee1954-3 data set
+#'
+#' Data from ...
+#'
+#' @docType data
+#'
+#' @usage data(allee1954_3)
+#'
+#' @format An 10x10 aggression matrix
+#'
+#' @keywords datasets
+#'
+#' @references Allee, W. C., and J. C. Dickinson Jr. "Dominance and
+#'   subordination in the smooth dogfish Mustelus canis (Mitchill)."
+#'   Physiological Zoology 27.4 (1954): 356-364.
+#'   (\href{https://doi.org/10.1086/physzool.27.4.30152372}{The University of Chicago Press})
+#'
+#' @source \href{https://datadryad.org/resource/doi:10.5061/dryad.f76f2}{Shizuka
+#'   D, McDonald DB (2015) Data from: The network motif architecture of
+#'   dominance hierarchies. Dryad Digital Repository}
+#'
+#' @examples
+#' data(allee1954_3)
+#' ec_power <- dom_ec(allee1954_3)
+"allee1954_3"
