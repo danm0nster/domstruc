14a9b4c273c349d1f8494f2c29425203153d2477
Dan Mønster <dan@m0nster.dk>
2019-12-07 14:30:29 +0100

    Used path to work with new version of lintr

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,9 +3,10 @@ library(domstruc)
 library(lintr)
 
 test_check("domstruc")
+path <- getwd()
 
 if (requireNamespace("lintr", quietly = TRUE)) {
   test_that("Package Style", {
-    lintr::expect_lint_free()
+    lintr::expect_lint_free(path = path, relative_path = FALSE)
   })
 }
