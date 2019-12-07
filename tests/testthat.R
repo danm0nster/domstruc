1c9491de6d788ff0b9f2fc444e838236cc5eba1c
Dan Mønster <dan@m0nster.dk>
2019-12-07 14:30:29 +0100

    Set path to work with new version of lintr

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,6 +6,6 @@ test_check("domstruc")
 
 if (requireNamespace("lintr", quietly = TRUE)) {
   test_that("Package Style", {
-    lintr::expect_lint_free()
+    lintr::expect_lint_free(path = "/Users/danm/Dropbox/AU/ResearchProjects/FightClub/Code/domstruc", relative_path = FALSE)
   })
 }
